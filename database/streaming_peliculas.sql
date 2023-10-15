9a5bf95fc21315a3b15dd43987343bf5ed8fc814
Simon Aceto <xsub123@gmail.com>
2023-10-15 17:26:41 -0300

    Actualizado archivo de BD

--- database/streaming_peliculas.sql
+++ database/streaming_peliculas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 26-09-2023 a las 04:26:00
+-- Tiempo de generación: 15-10-2023 a las 22:26:00
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -42,9 +42,12 @@ CREATE TABLE `peliculas` (
 --
 
 INSERT INTO `peliculas` (`id_pelicula`, `titulo`, `sinopsis`, `director`, `año_lanzamiento`, `cast`, `plataforma_id`) VALUES
-(4, 'Breaking Bad', 'JESSE WE NEED TO COOK', 'Vince Gilligan', '2008-01-20', 'INSERTAR CAST', 2),
-(5, 'Star Wars', 'EN UNA GALAXIA MUY LEJANA...', 'Lucas Trotacielos', '1977-12-25', 'TODO EL CAST DE STARWARS', 3),
-(6, 'El Señor de los Anillos: Los Anillos del Poder', 'MY PRECIOUS...', 'Peter Jackson', '2022-09-01', 'CAST DE LA SERIE', 4);
+(9, 'Better Call Saul', 'Ambientada en 2002, seis años antes de los acontecimientos relatadas en Breaking Bad, Better Call Saul es un spin-off centrado en el personaje de James \'Jimmy\' McGuill (Bob Odenkirk), antes de que asuma la identidad de Saul Goodman, un abogado corrupto con un humor políticamente incorrecto vinculado al mundo criminal que empieza a crear una importante red de contactos en los bajos mundos. La serie narra los acontecimientos que llevan a McGuill a convertirse en Saul antes de trabajar con Walter W', 'Vince Gilligan', '2015-02-08', 'Bob Odenkirk, Sea Reehorn, etc', 2),
+(14, 'Fionna & Cake', ' Fionna y Cake, con la ayuda del antiguo Rey Helado, Simon Petrikov, se embarcan en una aventura de salto multiversal y en un viaje de autodescubrimiento. Mientras tanto, un nuevo y poderoso antagonista, decidido a seguirles la pista y borrarlos de la existencia, acecha en las sombras.', 'Adam Muto', '2023-08-31', 'Cast Fionna', 3),
+(15, 'Breaking Bad', 'Breaking Bad sigue la transformación de Walter White, un profesor de química, en un narcotraficante mientras enfrenta su diagnóstico de cáncer terminal. La serie explora la decadencia moral y las consecuencias de sus decisiones.', 'Vince Gilligan', '2008-01-20', 'Cast BB', 2),
+(16, 'Testeo 2', 'debug', 'Director Generico', '2023-10-10', 'Lorem Ipsum Dolor Sit Amet', 4),
+(17, 'devtest', 'Lorem Ipsum dolor sit amet', 'John Lasseter', '2023-10-15', 'nmnmnmnmnmnmnmnmnmnmnm', 3),
+(18, 'Cars 2', 'sinopsis de cars 2', 'John Lasseter 2 ', '2023-10-07', 'CAST CARS ', 5);
 
 -- --------------------------------------------------------
 
@@ -66,14 +69,29 @@ CREATE TABLE `plataformas` (
 --
 
 INSERT INTO `plataformas` (`id_plataforma`, `nombre`, `enlace`, `tipo_contenido`, `disponibilidad_ar`, `precio`) VALUES
-(1, 'Netflix', 'https://www.netflix.com', 'Series, Peliculas, Documentales y Juegos', 1, 3990.24),
-(2, 'HBO Max', 'https://www.hbomax.com/', 'Series y Peliculas', 1, 1216.0),
-(3, 'Amazon Prime Video', 'https://www.primevideo.com', 'Series, Peliculas y Documentales', 1, 1390.0),
-(4, 'Star+', 'https://www.starplus.com', 'Series, Peliculas, Documentales y Deporte', 1, 1749.0),
-(5, 'Disney+', 'https://www.disneyplus.com', 'Series, Peliculas y Documentales', 1, 799.0),
-(6, 'paramount+', 'https://www.paramountplus.com', 'Series, Peliculas y Documentales', 1, 318.0),
-(7, 'MUBI', 'https://mubi.com', 'Peliculas y Documentales', 1, 599.0),
-(8, 'AppleTV', 'https://appleTV.com', 'Series, Peliculas y Documentales', 1, 8750.0);
+(2, 'Netflix', 'www.netflix.com', 'Series, Peliculas, Documentales', 1, 5000),
+(3, 'HBO Max', 'https://www.hbomax.com/', 'Series, Peliculas, Documentales', 1, 4000),
+(4, 'Amazon Prime Video', 'https://www.primevideo.com', 'Series, Peliculas, Documentales', 1, 4500),
+(5, 'Disney +', 'https://www.disneyplus.com/es-ar', 'Familiar', 1, 4000);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `username` varchar(200) NOT NULL,
+  `password` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `username`, `password`) VALUES
+(1, 'webadmin', '$2y$10$sIAxMnXh9.08IV3ZHfiMaODX//GYaxNiyFXrJxrz8PphHK.Mx1dyy');
 
 --
 -- Índices para tablas volcadas
@@ -93,6 +111,12 @@ ALTER TABLE `plataformas`
   ADD PRIMARY KEY (`id_plataforma`);
 
 --
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -100,13 +124,19 @@ ALTER TABLE `plataformas`
 -- AUTO_INCREMENT de la tabla `peliculas`
 --
 ALTER TABLE `peliculas`
-  MODIFY `id_pelicula` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id_pelicula` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `plataformas`
 --
 ALTER TABLE `plataformas`
-  MODIFY `id_plataforma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_plataforma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Restricciones para tablas volcadas
